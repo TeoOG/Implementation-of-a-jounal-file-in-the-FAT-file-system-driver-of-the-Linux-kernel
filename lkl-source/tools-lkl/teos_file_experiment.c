31b38808a1d573060a5e8235876d73f81d5fdde3
Theodoros Goltsios <48798079+TeoOG@users.noreply.github.com>
2023-07-12 04:52:04 +0300

    Add files via upload

---
+++ lkl-source/tools-lkl/teos_file_experiment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   
+
+   int a,b,c,d,res;
+
+   a=4;
+   b=7;
+   c=9;
+   d=9;
+
+   res = a+b+c+d;
+
+   printf("Hello, World, from Teo!");
+   return 0;
+}
---
+++ lkl-source/tools-lkl/teos_used_directory_experiment/teos_file_experiment2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   
+
+   int a,b,c,d,e,fres;
+
+   a=4;
+   b=7;
+   c=9;
+   d=9;
+
+   e=10;
+   f=1000;
+
+   res = a+b+c+d+e+f;
+
+   printf("Hello, World, from Teo!");
+   return 0;
+}
